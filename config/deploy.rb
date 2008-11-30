bd0fada4f4565fea2ad6e688bb2874e536391227
Thomas Hubertz <tjhubertz@gmail.com>
2008-11-30 21:29:57 +0100

    first on github

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,10 +4,13 @@ set :application, "token_auth"
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/var/www/apps/#{application}"
-set :user, "thub"
-set :runner,nil
+#set :user, "thub"
+#set :runner,nil
 set :use_sudo, false
 default_run_options[:pty] = true 
+
+set :mongrel_clean, true
+
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
